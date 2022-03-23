058d433147ad11902f505e5b08a424d97970aabb
Cassiano Colombo Peteck <cassianopeteck@gmail.com>
2022-03-23 10:32:57 -0300

    Base do projeto

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
+
 
 export default function App() {
+  function req(){
+    //Requisição vai aqui:
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(function(json){
+      alert(json.title);
+    })
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity onPress={()=>req()}><Text style={styles.text}>Clique Aqui!</Text></TouchableOpacity>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'purple',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize: 35,
+    color: 'white',
+    fontWeight: 'bold'
+  }
 });
